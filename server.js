d5f73a9892be253f1fadbd6a8f1c21ebaa7ff978
ziadabdelkader <zyad012ziad012@gmail.com>
2021-03-31 14:15:35 +0200

    add get data from API

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ app.use(bodyParser.json())
 const port = 3000;
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
@@ -21,23 +23,8 @@ app.use(express.static('website'));
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`)
 });
-
 ////////////////////////////////////////////////////////////////////////////////////
-// Setup empty JS object to act as endpoint for all routes
-// Express to run server and routes
 
-// Start up an instance of app
-
-/* Dependencies */
-/* Middleware*/
-
-//Here we are configuring express to use body-parser as middle-ware.
-// Cors for cross origin allowance
-
-// Initialize the main project folder
-
-// Spin up the server
-// Callback to debug
 
 // Initialize all route with a callback function
 
--- website/app.js
+++ website/app.js
@@ -1,17 +1,37 @@
 /* Global Variables */
+// Personal API Key for OpenWeatherMap API
+const apiKey = '9010d9daf8e32586a7033ec912159760';
+const generate = document.querySelector('#generate');
+const zipInput = document.querySelector('#zip');
+// Event listener to add function to existing HTML DOM element
+generate.addEventListener('click', generateClickHandler);
+
+function getURL(zipCode){
+    return 'http://api.openweathermap.org/data/2.5/weather?zip='+zipCode+'&appid='+apiKey;
+}
+/* Function to GET Web API Data*/
+async function getDataFromAPI(URL) {
+    let data = await fetch(URL);
+    data = await data.json();
+    return  data.main.temp;
+}
+/* Function called by event listener */
+async function generateClickHandler() {
+    const zipCode = zipInput.value;
+    const URL = getURL(zipCode);
+    const temp = await getDataFromAPI(URL);
+    console.log('temp = ',temp);
+}
 
 // Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+function getDate(){
+    const currentDate = new Date();
+    return currentDate.getMonth() + '.' + currentDate.getDate() + '.' + currentDate.getFullYear();
+}
 
 
-// Personal API Key for OpenWeatherMap API
 
-// Event listener to add function to existing HTML DOM element
 
-/* Function called by event listener */
-
-/* Function to GET Web API Data*/
 
 /* Function to POST data */
 
